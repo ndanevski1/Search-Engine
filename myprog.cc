85e4d77bd494185a726adb6fdfcb249c92bb0340
ReaLNeroM <vlade.maksimovski@gmail.com>
2021-04-07 13:59:08 -0400

    Add example file

---
+++ myprog.cc
@@ -0,0 +1,82 @@
+#include <xapian.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+// #include "common.h"
+
+using namespace std;
+
+void word_tokenize(string s, std::vector<string>& tokens){
+    std::string punctuation = "!\"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ ";
+    char is_punctuation[256];
+    for(int i = 0; i < 256; i++){
+        is_punctuation[i] = 0;
+    }
+    for(char c : punctuation){
+        is_punctuation[c] = true;
+    }
+
+    std::string curr_word = "";
+    cout << curr_word << '\n';
+    for(char c : s){
+        if(is_punctuation[c]){
+            if(curr_word != ""){
+                tokens.push_back(curr_word);
+            }
+            curr_word = "";
+        } else {
+            curr_word.push_back(c);
+        }
+    }
+    if(curr_word != ""){
+        tokens.push_back(curr_word);
+    }
+}
+
+int main(int argc, char **argv) {
+    if(argc < 2) {
+        cerr << "Usage: <idxname>" << endl;
+        exit(0);
+    }
+
+    char *idxname = argv[1];
+    Xapian::WritableDatabase db(idxname, Xapian::DB_CREATE_OR_OPEN);
+    Xapian::Document doc;
+
+    vector<string> words;
+    words.push_back("john's awefawe.");
+    words.push_back("a brave new world world");
+
+    long lineno = 0;
+    for(vector<string>::iterator it = words.begin();
+                                 it != words.end();
+                                 it++, lineno++) {
+        cout << "line " << lineno << ": ";
+        string &s = *it;
+
+        // perform tokenization
+        vector<string> tokens;
+        word_tokenize(s, tokens);
+
+        // populate the document
+        // value[0] will be the original line
+        doc.clear_terms();
+        doc.clear_values();
+        doc.add_value(0, string(s));
+        // document is the bag of terms
+        for(vector<string>::iterator it_token=tokens.begin();
+            it_token != tokens.end();
+            it_token++) {
+            string &token = *it_token;
+            cout << token << " ";
+            doc.add_term(token);
+        }
+        cout << endl;
+        db.add_document(doc);
+        if(lineno % 1000 == 0) {
+            db.commit();
+        }
+    }
+}
