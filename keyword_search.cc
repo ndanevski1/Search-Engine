3134bc32337e08284dd9c2f6d40b4dcf98908352
ndanevsk <ndanevsk@u.rochester.edu>
2021-04-08 04:54:07 -0400

    Created query sets in part 5

---
+++ keyword_search.cc
@@ -0,0 +1,67 @@
+#include <xapian.h>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <fstream>
+#include <cassert>
+#include <map>
+#include <cstdlib>
+#include <random>
+
+using namespace std;
+
+int get_random_number(int high) {
+    return rand() % high;
+}
+set<string> create_random_set(Xapian::Database db, int set_cardinality) {
+    set<string> s;
+    int doc_count = db.get_doccount();
+    for(int i = 0; i < set_cardinality; i++) {
+        int random_doc = get_random_number(doc_count);
+        Xapian::Document doc = db.get_document(random_doc);
+        int doc_terms_count = doc.termlist_count();
+        int random_term_index = get_random_number(doc_terms_count);
+        int j = 0;
+        string random_term = "";
+        for(auto it = doc.termlist_begin(); j < random_term_index && it != doc.termlist_end(); j++, it++)
+            random_term = *it;
+        s.insert(random_term);
+    }
+    return s;
+}
+
+
+vector<set<string>> create_sets_to_compare(Xapian::Database db, int set_cardinality, int number_of_sets) {
+    vector<set<string>> v;
+    for(int i = 0; i < number_of_sets; i++)
+        v.push_back(create_random_set(db, set_cardinality));
+    return v;
+}
+int main(int argc, char **argv) {
+    if(argc < 4) {
+        cerr << "Usage: keyword_search <index_name> <top-k> <set_cardinality>" << endl;
+        exit(0);
+    }
+    Xapian::Database db(argv[1]);
+    int top_k = atoi(argv[2]);
+    int set_cardinality = atoi(argv[3]);
+
+    set<string> query_set = create_random_set(db, set_cardinality);
+    int number_of_sets = 30;
+    vector<set<string>> sets_to_compare = create_sets_to_compare(db, set_cardinality, number_of_sets);
+
+    cout << "Printing query set: ";
+    for(auto s: query_set)
+        cout << s << " ";
+
+    cout << endl;
+    for(auto v: sets_to_compare) {
+        for(auto s: v)
+            cout << s << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
